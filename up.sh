519df0f126c57a44d6fd69cddad198f34cdd0d80
trgkien.vu <trgkien.vu@gmail.com>
2026-04-14 09:46:42 +0700

    Add check to skip fleet network creation if it already exists

--- up.sh
+++ up.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "Creating fleet network"
-docker network create fleet
+if docker network inspect fleet >/dev/null 2>&1; then
+  echo "Network fleet already exists, skipping creation."
+else
+  echo "Creating network fleet..."
+  docker network create fleet
+fi
 
 echo "Running compose up in fleet-traefik"
 docker compose up -d
